ed029175ab1b4c303938796fd4428c661bad7f0c
Vijay Rayapati <amnigos@mac>
2012-07-07 15:35:30 +0530

    Added comments for debugging purpose in case of failures during installtion of JMeter components on REMOTE MACHINES

--- install.sh
+++ install.sh
@@ -9,11 +9,18 @@ JMETER_VERSION=$3
 
 
 function install_jmeter_plugins() {
+    echo
+    echo "Installing JMeter Plugins"
+    echo 
     wget -q -O $REMOTE_HOME/JMeterPlugins.jar https://s3.amazonaws.com/jmeter-ec2/JMeterPlugins.jar
     mv $REMOTE_HOME/JMeterPlugins.jar $REMOTE_HOME/$JMETER_VERSION/lib/ext/
 }
 
 function install_mysql_driver() {
+
+    echo 
+    echo "Installing MySQL Drivers"
+    echo 
     wget -q -O $REMOTE_HOME/mysql-connector-java-5.1.16-bin.jar https://s3.amazonaws.com/jmeter-ec2/mysql-connector-java-5.1.16-bin.jar
     mv $REMOTE_HOME/mysql-connector-java-5.1.16-bin.jar $REMOTE_HOME/$JMETER_VERSION/lib/
 }
@@ -24,6 +31,8 @@ cd $REMOTE_HOME
 if [ $INSTALL_JAVA -eq 1 ] ; then
     # install java
 	
+    echo
+    echo "Installing JAVA, works only in Ubuntu"
 	#ubuntu
 	sudo apt-get update #update apt-get
 	sudo DEBIAN_FRONTEND=noninteractive apt-get -qqy install default-jre
@@ -55,7 +64,12 @@ jakarta-jmeter-2.5.1)
     ;;
 
 apache-jmeter-*)
+    
     # JMeter version 2.x
+    echo
+    echo "Installing $JMETER_VERSION from apache distribution"
+    echo
+
     wget -q -O $REMOTE_HOME/$JMETER_VERSION.tgz http://archive.apache.org/dist/jmeter/binaries/$JMETER_VERSION.tgz
     tar -xf $REMOTE_HOME/$JMETER_VERSION.tgz
     # install jmeter-plugins [http://code.google.com/p/jmeter-plugins/]
